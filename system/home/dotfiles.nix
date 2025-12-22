feeafb64291253141a86d79ca98c14016ba7872a
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-22 10:53:59 +0800

    Fixed scripts

--- system/home/dotfiles.nix
+++ system/home/dotfiles.nix
@@ -1,15 +1,17 @@
 { config, ... }:
 
 {
-  home.file.".config/alacritty".source = ../../alacritty;
-  home.file.".config/fastfetch".source = ../../fastfetch;
-  home.file.".config/hypr".source = ../../hyprland;
-  home.file.".config/mako".source = ../../mako;
-  home.file.".config/nvim".source = ../../neovim;
-  home.file.".config/rofi".source = ../../rofi;
-  home.file.".config/swayosd".source = ../../swayosd;
-  home.file.".config/waybar".source = ../../waybar;
-  home.file.".config/zellij".source = ../../zellij;
-  home.file.".p10k.zsh".source = ../../zsh/.p10k.zsh;
-  home.file.".zprofile".source = ../../zsh/.zprofile;
+  home.file = { 
+    ".config/alacritty".source = ../../alacritty;
+    ".config/fastfetch".source = ../../fastfetch;
+    ".config/hypr".source = ../../hyprland;
+    ".config/mako".source = ../../mako;
+    ".config/nvim".source = ../../neovim;
+    ".config/rofi".source = ../../rofi;
+    ".config/swayosd".source = ../../swayosd;
+    ".config/waybar".source = ../../waybar;
+    ".config/zellij".source = ../../zellij;
+    ".p10k.zsh".source = ../../zsh/.p10k.zsh;
+    ".zprofile".source = ../../zsh/.zprofile;
+  };
 }
