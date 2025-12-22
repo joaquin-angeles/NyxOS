fad0977c0c22b90c3836f334372014fce2a67286
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-22 12:26:01 +0800

    Updated neovim/lua/options.lua

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -14,4 +14,3 @@ vim.o.guicursor = "i:block-blinkon500-blinkoff500" -- Cursor configuration
 
 -- Transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
