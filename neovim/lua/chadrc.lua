4fd3e226c515fc53a11ba6268772ec68170b8976
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-27 12:54:17 +0800

    NvDash

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -13,11 +13,38 @@ M.base46 = {
     },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = {
+    load_on_startup = true,
+
+    buttons = {
+        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+        { txt = "󰈔  New File", keys = "fn", cmd = "enew" },
+        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+        { txt = "󰚥  Plugins", keys = "pl", cmd = "Lazy" },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+            content = "fit",
+        },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+}
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
 
 return M
