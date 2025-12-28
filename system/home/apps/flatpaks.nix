f6714a6318da541c14408df192ac45b9e5ee9437
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-28 23:51:53 +0800

    Fixed Flatpaks

--- system/home/apps/flatpaks.nix
+++ system/home/apps/flatpaks.nix
@@ -65,11 +65,11 @@
         "com.github.tchx84.Flatseal" = {
             Environment = {
                 "GTK_THEME" = "adw-gtk3-dark:dark";
-                Context.filesystems = [
-                    "!xdg-config/gtk-3.0"
-                    "!xdg-config/gtk-4.0"
-                ];
             };
+            Context.filesystems = [
+                "!xdg-config/gtk-3.0"
+                "!xdg-config/gtk-4.0"
+            ];
         };
 
         # Vesktop config
